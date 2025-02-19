673c8486813b2dffefc0302d6fdd382f93b0473d
padovan <padovanbackend@gmail.com>
2025-02-19 20:34:14 -0300

    Criando primeiros models

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+# Criando o Modelo de Contato do Site
+class Contact(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone = models.CharField(max_length=14) 
+    email = models.EmailField(max_length=250)
+    created_date = models.DateTimeField(default=timezone.now)
+    description = models.TextField(max_length=500)
